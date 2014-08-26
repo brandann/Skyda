8352fc41377ba93ec94c27f3504ef49375dfe514
Brandan Haertel <foilpudding@gmail.com>
2014-08-26 07:41:51 -0700

    Loaded Starting Code
    code is copied over from skyda-prototype

---
+++ Assets/Resources/Scripts/Map/Tokens.cs
@@ -0,0 +1,59 @@
+﻿
+public class Tokens {
+
+	public const char WATER = 'X';	
+	public const char GRASS= '_';	
+	public const char TREE= '0';	
+	public const char SAND= '*';	
+	public const char HOUSE = '=';	
+	public const char DOOR = '+';	
+	public const char PAVER = 'g';	
+	public const char HOUSEWALL = 'h';	
+//	public const char = 'i';	
+//	public const char = 'j';	
+//	public const char = 'k';	
+//	public const char = 'l';	
+//	public const char = 'm';	
+//	public const char = 'n';	
+//	public const char = 'o';	
+//	public const char = 'p';	
+//	public const char = 'q';	
+//	public const char = 'r';	
+//	public const char = 's';	
+//	public const char = 't';	
+//	public const char = 'u';	
+//	public const char = 'v';	
+//	public const char = 'w';	
+//	public const char = 'x';	
+//	public const char = 'y';	
+//	public const char = 'z';	
+	
+	public Tokens(){}
+	
+	public char getWATER() {return WATER;}
+	public char getGRASS() {return GRASS;}
+	public char getTREE() {return TREE;}
+	public char getSAND() {return SAND;}
+	public char getHOUSE() {return HOUSE;}
+	public char getDOOR() {return DOOR;}
+	public char getPAVER() {return PAVER;}
+	public char getHOUSEWALL() {return HOUSEWALL;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+//	public char get() {return ;}
+}
