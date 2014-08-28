753abdd585cdde3c81ebbe3afb5e4499fe58d46d
Brandan Haertel <foilpudding@gmail.com>
2014-08-28 14:50:15 -0700

    updated art
    main art and added random grass and rocks

--- Assets/Resources/Scripts/Map/Tiles/TreeTile.cs
+++ Assets/Resources/Scripts/Map/Tiles/TreeTile.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class TreeTile : MonoBehaviour {
 
+	public Sprite rock;
+	private SpriteRenderer sr;
 	// Use this for initialization
 	void Start () {
-	
+		sr = this.GetComponent<SpriteRenderer>();
+		int r = Random.Range(0,3);
+		switch(r){
+			case(0):
+				sr.sprite = rock;
+				break;
+		}
 	}
 	
 	// Update is called once per frame
