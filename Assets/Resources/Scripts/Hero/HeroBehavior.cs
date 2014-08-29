5feedd067cc2a343090695ef097ce9bc961f5c9d
Brandan Haertel <foilpudding@gmail.com>
2014-08-29 15:49:01 -0700

    reorganized and hero rotate

---
+++ Assets/Resources/Scripts/Hero/HeroBehavior.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Skyda{
+	public class HeroBehavior : MonoBehaviour {
+	
+		private float Speed = 3f;
+		Vector3 dir = Vector3.zero;
+		
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+			// move hero
+			float H = Input.GetAxis ("Horizontal");
+			float V = Input.GetAxis ("Vertical");
+			
+			bool pressed = false; 
+			if(Input.GetKey("up"))
+				pressed = true;
+			else if(Input.GetKey("down"))
+				pressed = true;
+			else if(Input.GetKey("left"))
+				pressed = true;
+			else if(Input.GetKey("right"))
+				pressed = true;
+			
+			Vector3 movement = new Vector3(Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+			
+			//if(movement.magnitude >= .1f){
+			if(pressed){
+				movement *= Speed;
+				this.transform.up = movement.normalized;
+				this.transform.position += this.transform.up * (Speed * Time.smoothDeltaTime);
+				if(H == 0 && V < 0){
+					this.transform.Rotate(new Vector3(0,180,0));
+				}
+			}		
+		}
+		
+		// hero collider
+		void OnTriggerEnter2D(Collider2D other){
+//			if(other.gameObject.tag == "water"){
+//				this.transform.Translate(new Vector3(-dir.x * Speed, -dir.y * Speed, 0));
+//			}
+		}
+	}
+}
